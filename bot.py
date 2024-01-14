a36d713198a6ae769ac0093727420ae1700fade6
Mark <faust.titov@gmail.com>
2024-01-14 19:24:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,48 +1,40 @@
-import os
+# 1.Импорт библиотек
 import logging
-import asyncio
+import os
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message             # ловим все обновления этого типа 
+from aiogram.filters.command import Command   # обрабатываем команды /start, /help и другие
 
-# from config import TOKEN
+# 2. Инициализация объектов
 TOKEN = os.getenv('TOKEN')
-
+bot = Bot(token=TOKEN)                        # Создаем объект бота
+dp = Dispatcher()                             # Создаем объект диспетчера. Все хэндлеры(обработчики) должны быть подключены к диспетчеру
 logging.basicConfig(level=logging.INFO)
 
-# Задание
+# Домашнее Задание
 # - Включить запись log в файл
 # - Бот принимает кириллицу отдаёт латиницу в соответствии с Приказом МИД по транслитерации
 # - Бот работает из-под docker контейнера
 
-# Создаем объект бота
-bot = Bot(token=TOKEN)
-
-# Создаем объект диспетчера
-# Все хэндлеры(обработчики) должны быть подключены к диспетчеру
-dp = Dispatcher()
-
-# Хэндлер на команду /start
-@dp.message(Command("start"))
-async def send_welcome(message: types.Message):
+# 3. Обработка/Хэндлер на команду /start
+@dp.message(Command(commands=['start']))
+async def proccess_command_start(message: Message):
     user_name = message.from_user.full_name
     user_id = message.from_user.id
-    text = f"Hello, {user_name}!"
-    logging.info(f"{user_name=} {user_id=} sent message: {message.text}")
-    await message.reply(text)
-
-# Хэндлер на любые сообщения
+    text = f'Привет, {user_name}!'
+    logging.info(f'{user_name} {user_id} запустил бота')
+    await bot.send_message(chat_id=user_id, text=text)
+    
+# 4. Обработка/Хэндлер на любые сообщения
 @dp.message()
-async def send_echo(message: types.Message):
+async def send_echo(message: Message):
     user_name = message.from_user.full_name
     user_id = message.from_user.id
-    text = message.text
-    logging.info(f"{user_name=} {user_id=} sent message: {text}")
-    await bot.send_message(user_id, text)
+    text = message.text 
+    logging.info(f'{user_name} {user_id}: {text}')
+    await message.answer(text=text)
 
-# Запуск процесса поллинга новых апдейтов
-async def main():
-    await dp.start_polling(bot)
-
+# 5. Запуск процесса пуллинга
 if __name__ == '__main__':
-    asyncio.run(main())
+    dp.run_polling(bot)
